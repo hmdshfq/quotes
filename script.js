089785f2ee9cb0a653f34b9b9466415e7a3926a6
Hammad <hmdshfq@outlook.com>
2022-12-04 00:07:53 +0100

    Rename title and add transition

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 logData()
 
 async function logData() {
+  var figureElement = document.querySelector('figure');
   var data = await fetchData()
   var quoteElement = document.querySelector('#quote')
   var authorElement = document.querySelector('#author')
   quoteElement.innerText = data.quote
   authorElement.innerText = data.author
+  figureElement.classList.add('active')
 }
 async function fetchData() {
   var randomNumber = Math.floor(Math.random() * 100)
