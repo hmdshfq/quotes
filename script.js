ff0981a28bb00ad3cce0f5f6ac12a6506eb9e6d0
Hammad <hmdshfq@outlook.com>
2023-08-16 11:18:29 +0200

    simplify script

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-logData()
+fetchData();
 
-async function logData() {
-  var figureElement = document.querySelector('figure');
-  var data = await fetchData()
-  var quoteElement = document.querySelector('#quote')
-  var authorElement = document.querySelector('#author')
-  quoteElement.innerText = data.quote
-  authorElement.innerText = data.author
-  figureElement.classList.add('active')
-}
 async function fetchData() {
-  var randomNumber = Math.floor(Math.random() * 100)
-  var response = await fetch('https://dummyjson.com/quotes/' + randomNumber)
-  var data = await response.json()
-  return data
+    var figureElement = document.querySelector('figure');
+    var quoteElement = document.querySelector('#quote');
+    var authorElement = document.querySelector('#author');
+    var randomNumber = Math.floor(Math.random() * 100);
+    var response = await fetch('https://dummyjson.com/quotes/' + randomNumber);
+    var data = await response.json();
+    quoteElement.innerText = data.quote;
+    authorElement.innerText = data.author;
+    figureElement.classList.add('active');
 }
